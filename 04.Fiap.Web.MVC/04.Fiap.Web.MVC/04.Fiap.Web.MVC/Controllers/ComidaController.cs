2affa85222a33e4ff8531fb0730b12cf11eda69a
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-17 22:41:07 -0300

    criado o projeto 04| simula cadastrar no banco, e listar

---
+++ 04.Fiap.Web.MVC/04.Fiap.Web.MVC/04.Fiap.Web.MVC/Controllers/ComidaController.cs
@@ -0,0 +1,35 @@
+﻿using _04.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04.Fiap.Web.MVC.Controllers
+{
+    public class ComidaController : Controller
+    {
+        //simular banco de dados
+        private static List<Comida> _banco = new List<Comida>();
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_banco);//envia a lista para a tela 
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Comida comida)
+        {
+            _banco.Add(comida);//cadastra no banco
+            //enviar msg de sucesso para apagina
+            TempData["msg"] = "Comida cadastrada!";
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
